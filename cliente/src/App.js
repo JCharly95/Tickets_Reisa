071510ee30c1f7567658c90f6d5b8f516bd44150
Luis Pablo Nieto <luispablonieto@gmail.com>
2020-06-06 22:26:02 -0500

    no acpetar

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Login from './componentes/auth/Login';
-import Admin from './componentes/admin/Obras/admin';
+import Admin from './componentes/admin/admin';
 import RecuperarCuenta from './componentes/auth/RecuperarCuenta';
 
-import ObrasState from './context/obras/obrasState';
 
 function App() {
 
   return (
-    <ObrasState>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Login} />
-          <Route exact path="/recuperarcuenta" component={RecuperarCuenta}/>
-          <Route exact path="/admin" component={Admin}/>
-        </Switch>
-      </Router>
-    </ObrasState>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/recuperarcuenta" component={RecuperarCuenta}/>
+        <Route exact path="/admin" component={Admin}/>
+      </Switch>
+    </Router>
   );
 }
 
