f57c1612ddcd8779506cf172d1cd5ece808ea015
JCharly95 <33488657+JCharly95@users.noreply.github.com>
2020-06-15 15:48:42 -0500

    Reconexion de inicio
    En este commit se reconecto el archivo de inicio.php

--- interfaces/Inicio/Inicio.php
+++ interfaces/Inicio/Inicio.php
@@ -1,60 +1,53 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>Barra de Inicio</title>
     <link rel="icon" href="img/mdb-favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/mdb.min.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.0/css/mdb.min.css" rel="stylesheet">
     <title>Inicio</title>
 </head>
 <body style="background: linear-gradient(to right, #34495e, #ebedef);">
-<nav class="navbar navbar-expand-lg navbar-dark primary-color">
-    <a class="navbar-brand" href="#"></a>
-
+    <nav class="navbar navbar-expand-lg navbar-dark primary-color">
+        <a class="navbar-brand" href="#"></a>
         <!-- Collapse button -->
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav" aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-
         <!-- Collapsible content -->
         <div class="collapse navbar-collapse" id="basicExampleNav">
-
             <!-- Links -->
             <ul class="navbar-nav mr-auto">
-            <a href="http://localhost/Web_3parcial/WEB_1/Inicio.php" class="btn blue-gradient btn-sm" ><i class="fas fa-home mr-1"></i>Inicio </a>
-            <a href="" class="btn blue-gradient btn-sm" ><i class="far fa-building mr-1"> </i>Obras</a>
-            <a href="" class="btn blue-gradient btn-sm" ><i class="far fa-bookmark mr-1"> </i>Reportes </a>
-            <a href="http://localhost/Web_3parcial/WEB_1/Tickets.php" class="btn blue-gradient btn-sm"><i class="fas fa-ticket-alt mr-1"> </i>Tickets </a>
-            <a href="http://localhost/Web_3parcial/WEB_1/usuarios.php" class="btn blue-gradient btn-sm" ><i class="fas fa-users mr-1"> </i>Usuarios </a>
-
+                <a href="Inicio.php" class="btn blue-gradient btn-sm" ><i class="fas fa-home mr-1"></i>Inicio </a>
+                <a href="" class="btn blue-gradient btn-sm" ><i class="far fa-building mr-1"> </i>Obras</a>
+                <a href="" class="btn blue-gradient btn-sm" ><i class="far fa-bookmark mr-1"> </i>Reportes </a>
+                <a href="" class="btn blue-gradient btn-sm"><i class="fas fa-ticket-alt mr-1"> </i>Tickets </a>
+                <a href="../usuarios/usuarios.php" class="btn blue-gradient btn-sm" ><i class="fas fa-users mr-1"> </i>Usuarios </a>
             </ul>
             <!-- Links -->
             <div class="md-form my-0">
                 <ul class="navbar-nav mr-auto">
-                <a href="http://localhost/Web_3parcial/WEB_1/admin.php" class="btn blue-gradient btn-sm" ><i class="fas fa-hammer mr-2"></i>Admin</a>
-                <a href="" class="btn blue-gradient btn-sm" ><i class="fas fa-sign-out-alt mr-2 "></i>Salir</a>
-            </ul>
+                    <a href="../Admin/admin.php" class="btn blue-gradient btn-sm" ><i class="fas fa-hammer mr-2"></i>Admin</a>
+                    <a href="" class="btn blue-gradient btn-sm" ><i class="fas fa-sign-out-alt mr-2 "></i>Salir</a>
+                </ul>
             </div>
-
         </div>
         <!-- Collapsible content -->
     </nav>
     <!--/.Navbar-->
-
-
-<div class="mx-auto card mt-5 gradient-card-header frozen-dreams-gradient" style="width: 30%;">
-    <h1 class="card-header text-center py-10">
-        <strong>WELCOMEBACK @ADMIN</strong>
-    </h1>
-</div>
-<script type="text/javascript" src="js/jquery.min.js"></script>
-<script type="text/javascript" src="js/popper.min.js"></script>
-<script type="text/javascript" src="js/bootstrap.min.js"></script>
-<script type="text/javascript" src="js/mdb.min.js"></script>
+    <div class="mx-auto card mt-5 gradient-card-header frozen-dreams-gradient" style="width: 30%;">
+        <h1 class="card-header text-center py-10">
+            <strong>WELCOMEBACK @ADMIN</strong>
+        </h1>
+    </div>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.0/js/mdb.min.js"></script>
 </body>
 </html>
